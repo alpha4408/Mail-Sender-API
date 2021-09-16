23d346bb136baff04579dab982def6bf900c8e0a
Alpha Ngwenya <angweny1@asu.edu>
2021-09-16 14:29:50 -0500

    Initial commit

---
+++ src/main/java/com/apsucodes/microservice/MailsenderApiApplication.java
@@ -0,0 +1,13 @@
+package com.apsucodes.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailsenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailsenderApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/apsucodes/microservice/controller/RegistrationController.java
@@ -0,0 +1,62 @@
+package com.apsucodes.microservice.controller;
+
+import com.apsucodes.microservice.model.User;
+import com.apsucodes.microservice.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private MailService notificationService;
+
+    @Autowired
+    private User user;
+
+
+    @RequestMapping("/send-mail")
+    public String send() {
+
+        /*
+         * Creating a User with the help of User class that we have declared and setting
+         * Email address of the sender.
+         */
+        user.setEmailAddress("angwenya16@gmail.com");  //Receiver's email address
+        /*
+         * Here we will call sendEmail() for Sending mail to the sender.
+         */
+        try {
+            notificationService.sendEmail(user);
+        } catch (MailException mailException) {
+            System.out.println(mailException);
+        }
+        return "Congratulations! Your mail has been send to the user.";
+    }
+
+
+    @RequestMapping("send-mail-attachment")
+    public String sendWithAttachment() throws MessagingException {
+
+        /*
+         * Creating a User with the help of User class that we have declared and setting
+         * Email address of the sender.
+         */
+        user.setEmailAddress("angwenya16@gmail.com"); //Receiver's email address
+
+        /*
+         * Here we will call sendEmailWithAttachment() for Sending mail to the sender
+         * that contains a attachment.
+         */
+        try {
+            notificationService.sendEmailWithAttachment(user);
+        } catch (MailException mailException) {
+            System.out.println(mailException);
+        }
+        return "Congratulations! Your mail has been send to the user.";
+    }
+}
